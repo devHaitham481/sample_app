1901f8724cab0e535942e8c69039338b70e050dc
devHaitham481 <devHaitham481@gmail.com>
2020-09-23 07:40:34 +0300

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem 'sqlite3', '~> 1.4'
 # Use bootstrap 
 gem 'bootstrap-sass' 
 # Use Puma as the app server
-gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
@@ -63,6 +62,9 @@ group :test do
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
 end
-
+group :production do 
+gem 'pg'
+gem 'puma', '~> 4.1'
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+test "should be invalid without a name" do
+  user = User.new(name: '')
+
+  assert user.invalid?, name: :blank
+end
+
+  # test "invalid signup information" do 
+  #   get signup_path 
+  #     @user = User.new(name: " ", 
+  #                              email: "user@invalid", 
+  #                              password: "foo",
+  #                              password_confirmation: "bar")
+  #     # post users_path(@user)
+  #     # assert_response :success
+  #     assert user.invalid? 
+     
+  #     # assert_template 'users/new'
+  #   end
+
+    test "invalid signup information" do 
+      get signup_path
+      assert_no_difference 'User.count' do 
+        post users_path, params:{ user: { name: " ",
+          email: "user@example",
+          password: "pass",
+          password_confirmation: "word" }}
+        end
+        assert_template 'users/new'
+      end
+
+  #   test "valid signup information" do 
+  #   get signup_path
+  #   assert_difference 'User.count', 1 do 
+  #   request_via_redirect users_path, user: { name: "Example User",
+  #                                         email: "user@example.com",
+  #                                         password: "password",
+  #                                         password_confirmation: "password" }
+  #   end
+  #     assert_template 'users/show'
+  # end
+
+end
