370a3643d2dc1a09637a27527669b8ec681405b4
devHaitham481 <devHaitham481@gmail.com>
2020-09-23 07:42:56 +0300

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -32,15 +32,16 @@ end
         assert_template 'users/new'
       end
 
-  #   test "valid signup information" do 
-  #   get signup_path
-  #   assert_difference 'User.count', 1 do 
-  #   request_via_redirect users_path, user: { name: "Example User",
-  #                                         email: "user@example.com",
-  #                                         password: "password",
-  #                                         password_confirmation: "password" }
-  #   end
-  #     assert_template 'users/show'
-  # end
+    test "valid signup information" do 
+    get signup_path
+    assert_difference 'User.count', 1 do 
+    post users_path, params:{user: { name: "Example User",
+                                          email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password" }}
+    follow_redirect!
+    end
+      assert_template 'users/show'
+  end
 
 end
